b5960f2c7918242b7a094dd8355c335258b89250
Matt Sullivan <mattsully01@gmail.com>
2025-05-20 15:54:14 -0600

    pushing code for use on laptop

--- main.py
+++ main.py
@@ -12,14 +12,41 @@ from constants import(
     ASTEROID_MAX_RADIUS
     )
 
-def main():
-    pygame.init()
-    print("Starting Asteroids!")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+from player import Player
 
+def main():
+
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+    print("Starting Asteroids!")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #group setup
+    updatables = []
+    drawables = []
+    
+    player = Player(x = SCREEN_WIDTH / 2 , y = SCREEN_HEIGHT / 2)
+    updatables.append(player)
+    drawables.append(player)
+    
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
         screen.fill(color="black")
+
+        for obj in updatables:
+            obj.update(dt)
+
+        for obj in drawables:
+            obj.draw(screen)
+
         pygame.display.flip()
+        dt += clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
